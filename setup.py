87c4b5161b6547282e35e4660a680544dc8581ff
janSani <santipt50@gmail.com>
2025-03-05 20:11:54 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     author_email="<santipt50@gmail.com>",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=['transformations','numpy'],
 
